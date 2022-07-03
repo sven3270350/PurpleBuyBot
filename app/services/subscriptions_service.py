bd73089085bd1ab908e20d5c090dd2b095144709
sven3270350 <sven3270350@gmail.com>
2022-07-03 18:20:36 +0000

    subscriptions class placeholder

---
+++ app/services/subscriptions_service.py
@@ -0,0 +1,17 @@
+
+class SubscriptionService:
+    
+    # get group subscriptions
+    # is subscription active
+    # create subscription
+
+    def get_group_subscriptions(self, group_id):
+        pass
+
+    def is_subscription_active(self, subscription_id):
+        pass
+
+    def create_subscription(self, group_id, wallet_id, start_date, end_date, subscription_type, subscription_status, tx_hash):
+        pass
+
+    
