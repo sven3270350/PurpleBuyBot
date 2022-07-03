2b10a07f2ae1f35c43e0b6e440ecd419b12cb1a8
sven3270350 <sven3270350@gmail.com>
2022-07-03 18:13:50 +0000

    biggest buy service class outline

---
+++ app/services/biggest_buy_service.py
@@ -0,0 +1,23 @@
+
+class BiggestBuyService:
+
+    # create a new campaigns,
+    # get all campaigns,
+    # get active campaigns,
+    # get campaigns transactions
+    # get campaigns winners
+
+    def create_campaign(self, group_id, start_time, end_time, count_down, campaign_status, campaing_winner, minimum_buy_amount, prize):
+        pass
+
+    def get_all_campaigns(self):
+        pass
+
+    def get_active_campaigns(self):
+        pass
+
+    def get_campaign_transactions(self, campaign_id):
+        pass
+
+    def get_campaign_winners(self, campaign_id):
+        pass
