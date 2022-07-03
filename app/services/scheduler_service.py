e149f2db51cdc6ae1af5ece1f89d70bb812005a5
sven3270350 <sven3270350@gmail.com>
2022-07-03 18:20:25 +0000

    scheduler class placeholder

---
+++ app/services/scheduler_service.py
@@ -0,0 +1,7 @@
+from flask_rq import RQ
+class SchedularService:
+    
+    # schedule a campaign to start at a specific time
+
+    def schedule_campaign(self, group_id, campaign_type, start_time):
+        pass
