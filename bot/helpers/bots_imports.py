b6e62b03cc31ee934d36992c7a0fb1ae0d4b18eb
sven3270350 <sven3270350@gmail.com>
2022-07-05 23:20:27 +0000

    fix

--- bot/helpers/bots_imports.py
+++ bot/helpers/bots_imports.py
@@ -2,5 +2,5 @@ import os
 import telegram
 from telegram.ext import Updater, MessageHandler, Filters
 from telegram.ext import CommandHandler
-from bot.app import get_info
+from app import get_info
 from decouple import config
