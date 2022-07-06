35b668dd8b343db717ede12de11c17bde3f8b669
sven3270350 <sven3270350@gmail.com>
2022-07-06 12:39:12 +0000

    create bot service class

---
+++ bot/services/bot_service.py
@@ -0,0 +1,12 @@
+from services.web3_service import Web3Service
+from models import db, Group, Wallet
+
+
+class BotService:
+
+    def create_new_bot_user(self, chat_id, chat_title):
+        group_instance = Group(chat_id, chat_title)
+        (wallet_address, wallet_private_key) = Web3Service().create_wallet()
+        group_instance.wallet = Wallet(wallet_address, wallet_private_key)
+        db.session.add(group_instance)
+        db.session.commit()
