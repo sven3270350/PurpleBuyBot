831800b26155809f7aa3aec3581518216ee829de
sven3270350 <sven3270350@gmail.com>
2022-07-06 10:58:01 +0000

    add templates file

---
+++ bot/helpers/templates.py
@@ -0,0 +1,51 @@
+start_template = '''
+    <b>Welcome to Biggest Buy Bot!</b>
+
+Follow these simple steps to get started:
+
+<b>Supported Commands:</b>
+1) /help - Show supported commands
+2) /add_token - Add the token to be monitored requires token address
+3) /remove_token - Remove monitored token
+4) /start_buy_contest - Initiate a biggest buy contest
+5) /raffle_on - Start raffle buy contest
+6) /subscribe - Subscribe to premium to remove ads
+7) /chains - Show a list of supported chains
+
+<b>Adding a token:</b>
+Use command /add_token {token_address} or just /add_token then follow the prompt to complete
+
+<b>Removing a token:</b>
+Use command /remove_token {token_address} or just /remove_token
+
+<b>Run Contest:</b>
+To run a Buy contest, use /stat_buy_contest and follow the prompt
+For Raffle contests, use /raffle_on and follow the prompt
+
+<i>Note: Only one contest (Buy or Raffle) can be active per period.</i>
+    '''
+
+help_template = '''
+Follow these simple steps to get started:
+
+<b>Supported Commands:</b>
+1) /help - Show supported commands
+2) /add_token - Add the token to be monitored requires token address
+3) /remove_token - Remove monitored token
+4) /start_buy_contest - Initiate a biggest buy contest
+5) /raffle_on - Start raffle buy contest
+6) /subscribe - Subscribe to premium to remove ads
+7) /chains - Show a list of supported chains
+
+<b>Adding a token:</b>
+Use command /add_token {token_address} or just /add_token then follow the prompt to complete
+
+<b>Removing a token:</b>
+Use command /remove_token {token_address} or just /remove_token
+
+<b>Run Contest:</b>
+To run a Buy contest, use /stat_buy_contest and follow the prompt
+For Raffle contests, use /raffle_on and follow the prompt
+
+<i>Note: Only one contest (Buy or Raffle) can be active per period.</i>
+'''
