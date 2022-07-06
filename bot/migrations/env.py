e750e6c08ac1cedc19c9d20bd7a295a1bf500d09
sven3270350 <sven3270350@gmail.com>
2022-07-06 10:58:21 +0000

    include db models in migrations

--- bot/migrations/env.py
+++ bot/migrations/env.py
@@ -7,7 +7,7 @@ from flask import current_app
 
 from alembic import context
 
-from models import *
+from bot.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
