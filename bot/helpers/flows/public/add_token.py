f99d515b70a1eb619b3c6106ce8653bd5a252a73
sven3270350 <sven3270350@gmail.com>
2022-07-12 08:02:33 +0000

    imports for add token

--- bot/helpers/flows/public/add_token.py
+++ bot/helpers/flows/public/add_token.py
@@ -7,9 +7,9 @@
 # 6. Request for confirmation
 # 7. Write to DB
 
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, Dispatcher, CommandHandler, Filters
 from telegram.utils import helpers
 from telegram import Update, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 from services.bot_service import BotService
 from helpers.utils import is_private_chat, is_group_admin
-from helpers.templates import start_template, start_template_private, start_added_to_group
+# from helpers.templates import 
