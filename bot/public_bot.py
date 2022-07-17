d977269a6e03a25eaba8a0327ad40acece9eaa8c
sven3270350 <sven3270350@gmail.com>
2022-07-17 23:51:55 +0000

    minor updates

--- bot/public_bot.py
+++ bot/public_bot.py
@@ -36,23 +36,31 @@ def remove_tokens(update: Update, context: CallbackContext):
 
 
 def list_tracked_tokens(update: Update, context: CallbackContext):
-    chat_data = context.chat_data
-    tracked_tokens: list[TrackedToken] = BotService(
-    ).get_tracked_tokens(chat_data['group_id'])
-    message = "<b>You're Tracking:</b>\n\n"
+    group_id = context.chat_data.get('group_id', None)
+    group_title = context.bot.get_chat(group_id).title
 
-    if len(tracked_tokens) == 0:
-        message += '<i>You currently are not tracking any tokens.</i>\n'
-        message += '<i>Use the /add_tokens command to add tokens to your list.</i>'
+    if is_private_chat(update):
+        if not BotService().is_group_in_focus(update, context):
+            return
 
-    else:
-        # loop through the supported chains and add them to the message with numbered list
-        for token in tracked_tokens:
-            message += f"<b>{token.token_symbol}</b>\n"
-            message += f"<b>Token Address: </b><i>{token.token_address}</i>\n"
-            message += f"<b>Pair Address: <i>{token.pair_address}</i>\n\n"
+        tracked_tokens: list[TrackedToken] = BotService(
+        ).get_tracked_tokens_by_group_id(group_id)
+        message = f"<b>You're Tracking ({group_title}):</b>\n\n"
 
-    update.message.reply_text(message, parse_mode=ParseMode.HTML)
+        if len(tracked_tokens) == 0:
+            message += '<i>You currently are not tracking any tokens.</i>\n'
+            message += '<i>Use the /add_tokens command to add tokens to your list.</i>'
+
+        else:
+            # loop through the supported chains and add them to the message with numbered list
+            for token in tracked_tokens:
+                message += f"<b>{token.token_name} on {token.chain_name}</b>\n"
+                message += f"<i><b>Token Address: </b>{token.token_address}</i>\n"
+                message += f"<i><b>Pair Address: </b>{token.pair_address}</i>\n"
+                message += f"<i><b>Pair: </b>{token.pair_symbol}</i>\n"
+                message += f"<i><b>Dex: </b>{token.exchange_name}</i>\n\n"
+
+        update.message.reply_text(message, parse_mode=ParseMode.HTML)
 
 
 def start_buy_contest(update: Update, context: CallbackContext):
@@ -78,25 +86,27 @@ def chains(update: Update, context: CallbackContext):
     ).get_supported_chains()
     message = '<b>Supported Chains:</b>\n\n'
 
-    if len(supported_chains) == 0:
-        message += '<i>No supported chains found.</i>'
-    else:
-        # loop through the supported chains and add them to the message with numbered list
-        for chain in supported_chains:
-            message += f'<b>{chain.chain_name}</b>\n'
+    if is_private_chat(update):
 
-            dexes: list[SupportedExchange] = BotService(
-            ).get_supported_dexes(chain.id)
-            pairs: list[SupportedPairs] = BotService(
-            ).get_supported_pairs(chain.id)
+        if len(supported_chains) == 0:
+            message += '<i>No supported chains found.</i>'
+        else:
+            # loop through the supported chains and add them to the message with numbered list
+            for chain in supported_chains:
+                message += f'<b>{chain.chain_name}</b>\n'
 
-            message += "⮕ <i><b>Dex: </b>" + \
-                ("{}, " * len(dexes)).format(*
-                                             [dex.exchange_name for dex in dexes]) + "</i>\n"
-            message += "⮕ <i><b>Pair: </b>" + ("{}, " * len(pairs)).format(
-                *[pair.pair_name for pair in pairs]) + "</i>\n\n"
+                dexes: list[SupportedExchange] = BotService(
+                ).get_supported_dexes(chain.id)
+                pairs: list[SupportedPairs] = BotService(
+                ).get_supported_pairs(chain.id)
 
-    update.message.reply_text(message, parse_mode=ParseMode.HTML)
+                message += "⮕ <i><b>Dex: </b>" + \
+                    ("{}, " * len(dexes)).format(*
+                                                 [dex.exchange_name for dex in dexes]) + "</i>\n"
+                message += "⮕ <i><b>Pair: </b>" + ("{}, " * len(pairs)).format(
+                    *[pair.pair_name for pair in pairs]) + "</i>\n\n"
+
+        update.message.reply_text(message, parse_mode=ParseMode.HTML)
 
 
 # call handlers for start commands
