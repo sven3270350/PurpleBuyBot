d6d8089e827c51dea8c2b18a678732acd128325a
sven3270350 <sven3270350@gmail.com>
2022-07-17 19:07:06 +0000

    remove 'add tokens' in public bot

--- bot/public_bot.py
+++ bot/public_bot.py
@@ -29,40 +29,6 @@ def help(update: Update, context: CallbackContext):
                                   parse_mode=ParseMode.HTML)
 
 
-def add_tokens(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    message = update.message.text
-    args = extract_params(message)
-
-    if len(args) == 0:
-        # no arg provided, request for token address
-        context.bot.send_message(chat_id=chat_id, text=help_template,
-                                 parse_mode=ParseMode.HTML)
-    elif len(args) == 1:
-        # one arg provided, check if it's a valid token address
-        token_address = args[0]
-        if is_address(token_address):
-            tracked_tokens = TrackedToken(
-                token_address=token_address,
-                group_id=str(chat_id)
-            )
-            db.session.add(tracked_tokens)
-            context.user_data['tracked_tokens'] = token_address
-            # TODO: check if token is already tracked
-            pass
-        else:
-            error_message = '<b>❌Invalid token address: </b>\n\n'
-            error_message += f'<i>{token_address}</i>\n'
-            error_message += '<i>Please provide a valid token address.</i>'
-            update.message.reply_text(
-                error_message, parse_mode=ParseMode.HTML)
-    else:
-        error_message = '<b>❌Invalid command params: </b>\n\n'
-        error_message += 'Use command /add_token {token_address} or just /add_token then follow the prompt to complete'
-        update.message.reply_text(
-            error_message, parse_mode=ParseMode.HTML)
-
-
 def remove_tokens(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     context.bot.send_message(chat_id=chat_id, text=help_template,
@@ -70,23 +36,21 @@ def remove_tokens(update: Update, context: CallbackContext):
 
 
 def list_tracked_tokens(update: Update, context: CallbackContext):
-    chat_id = update.effective_chat.id
-    tracked_tokens = BotService().get_tracked_tokens(chat_id)
+    chat_data = context.chat_data
+    tracked_tokens: list[TrackedToken] = BotService(
+    ).get_tracked_tokens(chat_data['group_id'])
     message = "<b>You're Tracking:</b>\n\n"
-    print(db.session.query(TrackedToken).filter(
-        TrackedToken.group_id == str(chat_id)).first())
 
     if len(tracked_tokens) == 0:
         message += '<i>You currently are not tracking any tokens.</i>\n'
         message += '<i>Use the /add_tokens command to add tokens to your list.</i>'
-        message += f"<i>{context.user_data['tracked_tokens']}</i>"
 
     else:
         # loop through the supported chains and add them to the message with numbered list
         for token in tracked_tokens:
-            message += f"<b>{token.token_symbol} on {token.token_name}</b>\n"
-            message += f" <i>{token.token_address}</i>\n"
-            message += f" <i>{token.pair_address}</i>\n"
+            message += f"<b>{token.token_symbol}</b>\n"
+            message += f"<b>Token Address: </b><i>{token.token_address}</i>\n"
+            message += f"<b>Pair Address: <i>{token.pair_address}</i>\n\n"
 
     update.message.reply_text(message, parse_mode=ParseMode.HTML)
 
@@ -128,7 +92,7 @@ def chains(update: Update, context: CallbackContext):
 
             message += "⮕ <i><b>Dex: </b>" + \
                 ("{}, " * len(dexes)).format(*
-                                            [dex.exchange_name for dex in dexes]) + "</i>\n"
+                                             [dex.exchange_name for dex in dexes]) + "</i>\n"
             message += "⮕ <i><b>Pair: </b>" + ("{}, " * len(pairs)).format(
                 *[pair.pair_name for pair in pairs]) + "</i>\n\n"
 
@@ -141,7 +105,6 @@ AddToken(dispatcher)
 
 # handlers for the commands
 dispatcher.add_handler(CommandHandler("help", help))
-# dispatcher.add_handler(CommandHandler("add_token", add_tokens))
 dispatcher.add_handler(CommandHandler("remove_tokens", remove_tokens))
 dispatcher.add_handler(CommandHandler("tracked_tokens", list_tracked_tokens))
 dispatcher.add_handler(CommandHandler("buy_contest", start_buy_contest))
