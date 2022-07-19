a99b0ee9cf77cf4167770030ffa7da326728cb2c
sven3270350 <sven3270350@gmail.com>
2022-07-19 19:48:54 +0000

    move commands to general handlers

--- bot/public_bot.py
+++ bot/public_bot.py
@@ -1,15 +1,14 @@
 from helpers.flows.public.remove_token import RemoveToken
 from helpers.flows.public.add_token import AddToken
 from helpers.flows.public.start import StartBot
+from helpers.flows.public.general import GeneralHandler
 from helpers.bots_imports import *
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
-from models import db, TrackedToken, SupportedChain, SupportedExchange, SupportedPairs
-from helpers.utils import extract_params, is_private_chat
+from models import SupportedChain, SupportedExchange, SupportedPairs
+from helpers.utils import is_private_chat
 from services.bot_service import BotService
-from services.biggest_buy_service import BiggestBuyService
 from services.bot_service import BotService
-from eth_utils import is_address
 from helpers.templates import help_template
 
 
@@ -22,12 +21,6 @@ bot = telegram.Bot(token=telegram_bot_token)
 
 updater = Updater(token=telegram_bot_token, use_context=True)
 dispatcher = updater.dispatcher
-
-
-def help(update: Update, context: CallbackContext):
-    if is_private_chat(update):
-        update.message.reply_text(help_template,
-                                  parse_mode=ParseMode.HTML)
 
 
 def start_buy_contest(update: Update, context: CallbackContext):
@@ -47,46 +40,16 @@ def subscribe(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=chat_id, text=help_template,
                              parse_mode=ParseMode.HTML)
 
-
-def chains(update: Update, context: CallbackContext):
-    supported_chains: list[SupportedChain] = BotService(
-    ).get_supported_chains()
-    message = '<b>Supported Chains:</b>\n\n'
-
-    if is_private_chat(update):
-
-        if len(supported_chains) == 0:
-            message += '<i>No supported chains found.</i>'
-        else:
-            # loop through the supported chains and add them to the message with numbered list
-            for chain in supported_chains:
-                message += f'<b>{chain.chain_name}</b>\n'
-
-                dexes: list[SupportedExchange] = BotService(
-                ).get_supported_dexes(chain.id)
-                pairs: list[SupportedPairs] = BotService(
-                ).get_supported_pairs(chain.id)
-
-                message += "⮕ <i><b>Dex: </b>" + \
-                    ("{}, " * len(dexes)).format(*
-                                                 [dex.exchange_name for dex in dexes]) + "</i>\n"
-                message += "⮕ <i><b>Pair: </b>" + ("{}, " * len(pairs)).format(
-                    *[pair.pair_name for pair in pairs]) + "</i>\n\n"
-
-        update.message.reply_text(message, parse_mode=ParseMode.HTML)
-
-
 # call handlers for start commands
-StartBot().call_start_handlers(dispatcher)
+StartBot(dispatcher)
 AddToken(dispatcher)
 RemoveToken(dispatcher)
+GeneralHandler(dispatcher)
 
 # handlers for the commands
-dispatcher.add_handler(CommandHandler("help", help))
 dispatcher.add_handler(CommandHandler("buy_contest", start_buy_contest))
 dispatcher.add_handler(CommandHandler("raffle_contest", start_raffle_contest))
 dispatcher.add_handler(CommandHandler("subscribe", subscribe))
-dispatcher.add_handler(CommandHandler("chains", chains))
 
 #
 updater.start_polling()
