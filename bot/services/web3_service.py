7827c5006a7d443b08eef87c0731a137ba492310
sven3270350 <sven3270350@gmail.com>
2022-08-04 11:46:55 +0000

    add get balance and transfer balance

--- bot/services/web3_service.py
+++ bot/services/web3_service.py
@@ -7,6 +7,8 @@ from models import db, Wallet, Subscription, TrackedToken, ActiveCompetition, Bi
 from decouple import config
 from datetime import datetime, timedelta
 
+admin_wallet = config('ADMIN_WALLET_ADDRESS')
+
 
 class Web3Service:
     # create a unique wallet address for each group and save the address and private key
@@ -18,70 +20,38 @@ class Web3Service:
         wallet_address = wallet.address
         return wallet_address, wallet_private_key
 
-    # listen for new deposits on wallet address and move the funds to the admin wallet
-    def listen_for_deposits(self, group_id):
-        # get the wallet address and private key from the database
-        wallet_instance = Wallet.query.filter_by(group_id=group_id).first()
-        wallet_address = wallet_instance.wallet_address
-        wallet_private_key = wallet_instance.wallet_private_key
+    def get_wallet_balance(self, chain_id, wallet_address):
+        provider = AppConfigs().get_provider(chain_id)
+        web3 = Web3(Web3.HTTPProvider(provider))
+        return web3.eth.get_balance(Web3.toChecksumAddress(wallet_address))
 
-        # create a web3 instance with the wallet address and private key
-        web3 = Web3(Web3.WebsocketProvider(config('WEB3_PROVIDER')))
+    def transfer_balance_to_admin(self, chain_id, wallet_id):
+        provider = AppConfigs().get_provider(chain_id)
+        web3 = Web3(Web3.HTTPProvider(provider))
 
-        web3.middleware_stack.inject(geth_poa_middleware, layer=0)
+        wallet: Wallet = Wallet.query.filter_by(id=wallet_id).first()
 
-        # listen for new deposits on the wallet address
-        web3.eth.watch(wallet_address)
-        # get the admin wallet address from env file
-        admin_wallet_address = config('ADMIN_WALLET_ADDRESS')
-        #  create a new transaction to move the funds to the admin wallet
-        transaction = {
-            'from': wallet_address,
-            'to': admin_wallet_address,
-            'value': web3.eth.getBalance(wallet_address)
+        web3.middleware_onion.inject(geth_poa_middleware, layer=0)
+
+        web3.eth.default_account = wallet.wallet_address
+
+        nonce = web3.eth.get_transaction_count(wallet.wallet_address)
+
+        # build a transaction in a dictionary
+        tx = {
+            'nonce': nonce,
+            'to':  Web3.toChecksumAddress(admin_wallet),
+            'value': web3.eth.get_balance(Web3.toChecksumAddress(wallet.wallet_address)),
+            'gas': 2000000,
+            'gasPrice': web3.toWei('50', 'gwei')
         }
 
-        # sign the transaction with the private key
-        signed_transaction = web3.eth.account.signTransaction(
-            transaction, wallet_private_key)
+        # sign the transaction
+        signed_tx = web3.eth.account.sign_transaction(
+            tx, wallet.wallet_private_key)
 
-        # send the transaction
-        tx_hash = web3.eth.sendRawTransaction(
-            signed_transaction.rawTransaction)
-        # get the transaction hash
-        tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
-
-        # if transaction completed successfully, create a weekly subscription for the group for which the end date is set to the next week
-        if tx_receipt.status == 1:
-            subscription_instance = Subscription(group_id, wallet_address,
-                                                 datetime.now(), datetime.now() + timedelta(days=7), 'weekly', 'active', tx_hash)
-            db.session.add(subscription_instance)
-            db.session.commit()
-        return tx_receipt
-
-    # for each each token tracked by the group, listen for new buys on the pair and save the transaction to the database for the active campaign
-    def listen_for_buys(self, group_id):
-        # get group's tracked tokens from the database
-        tokens = TrackedToken.query.filter_by(group_id=group_id).all()
-        minimum_buy_amount = AppConfigs().get_minimum_buy_amount()
-
-        # get active competion from the database
-        active_competition = ActiveCompetition.query.filter_by(
-            group_id=group_id).first()
-        competition_type = active_competition.competition_type
-        competition_id = active_competition.competition_id
-
-        # if competition type is BiggestBuy, get the minimum buy amount from the database
-        if competition_type == 'BiggestBuy':
-            biggest_buy_campaign = BiggestBuyCampaign.query.filter_by(
-                id=competition_id).first()
-            minimum_buy_amount = biggest_buy_campaign.minimum_buy_amount
-
-        # for each token, listen for new buys on the pair
-        for token in tokens:
-            # get pair address
-            pair_address = token.pair_address
-            pass
+        # send transaction
+        web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
     # given token1 address, token2 address, and chain return the token1 name, token1 symbol, token1 decimal and pair address
 
@@ -139,3 +109,26 @@ class Web3Service:
         web3 = Web3(Web3.HTTPProvider(provider))
         block = web3.eth.get_block(block_number)
         return datetime.fromtimestamp(block.timestamp)
+
+    def get_swap_event_from_pair_address(self, chain_id, pair_address):
+        provider = AppConfigs().get_provider(chain_id)
+        web3 = Web3(Web3.HTTPProvider(provider))
+        pair = web3.eth.contract(
+            address=pair_address, abi=AppConfigs().get_pair_abi()
+        )
+        return pair.events.Swap.createFilter(fromBlock='latest')
+
+    def swap_even_handler(self, pair: str, data: dict):
+        amount_0_in = data.get('amount0In')
+        amount_1_out = data.get('amount1Out')
+        to = data.get('to')
+
+        if amount_0_in > 0:
+            return {
+                'pair': pair,
+                'amount0In': amount_0_in,
+                'amount1Out': amount_1_out,
+                'to': to
+            }
+        else:
+            return None
