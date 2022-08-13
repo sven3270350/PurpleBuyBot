9b6cb59211adeb832a0e3251c004a4593c88282d
sven3270350 <sven3270350@gmail.com>
2022-08-13 06:04:28 +0000

    update utils

---
+++ nodeJs/utils/index.js
@@ -0,0 +1,129 @@
+const queries = require("../db/queries");
+const appConfig = require("./app_config");
+const UniswapV2Pair = require("./IUniswapV2Pair.json");
+const ERC20 = require("./erc20abi.json");
+const Web3 = require("web3");
+
+const wss = (provider) => new Web3(provider);
+
+const selectTrackedToken = async (trackedToken, contract) => {
+  const token0 = await getToken0(contract);
+  const token1 = await getToken1(contract);
+
+  if (trackedToken === token0) {
+    return { token: 0, address: token0 };
+  } else {
+    return { token: 1, address: token1 };
+  }
+};
+
+const getToken0 = async (contract) => {
+  return contract.methods.token0().call();
+};
+
+const getToken1 = async (contract) => {
+  return contract.methods.token1().call();
+};
+
+const hasActiveSubscription = async (groupId) => {
+  const activeSubscription = await queries.getActiveSubscriptionByGroupId(
+    groupId
+  );
+  return activeSubscription.length > 0;
+};
+
+const getTokenDecimals = async (address, chain) => {
+  const web3 = wss(appConfig.getProvider(chain));
+  const token = new web3.eth.Contract(ERC20, address);
+  return token.methods.decimals().call();
+};
+
+const swapHanlder = async (contract, trackedToken, data, callback) => {
+  const token_address = trackedToken.token_address.toLowerCase();
+  const decimals = trackedToken.token_decimals;
+  const chainId = trackedToken.chain_id;
+
+  const selectedTrackedToken = await selectTrackedToken(
+    token_address,
+    contract
+  );
+
+  let amountIn = 0;
+  let amountOut = 0;
+
+  if (selectedTrackedToken.token === 1) {
+    const token1Decimals = await getTokenDecimals(
+      selectedTrackedToken.address,
+      chainId
+    );
+
+    amountIn = Web3.utils.fromWei(
+      data.returnValues.amount1In,
+      decimalsToUnit(token1Decimals)
+    );
+
+    amountOut = Web3.utils.fromWei(
+      data.returnValues.amount0Out,
+      decimalsToUnit(decimals)
+    );
+  } else {
+    const token0Decimals = await getTokenDecimals(
+      selectedTrackedToken.address,
+      chainId
+    );
+
+    amountIn = Web3.utils.fromWei(
+      data.returnValues.amount0In,
+      decimalsToUnit(token0Decimals)
+    );
+
+    amountOut = Web3.utils.fromWei(
+      data.returnValues.amount1Out,
+      decimalsToUnit(decimals)
+    );
+  }
+
+  const to = data.returnValues.to;
+
+  if (amountIn > 0 || amountOut > 0) {
+    console.log(data.returnValues);
+    callback(trackedToken, amountIn, amountOut, to);
+  }
+};
+
+const addressInSubscription = (address, subscription) => {
+  const length = Object.keys(subscription).length;
+  if (length === 0) return false;
+  return !!subscription.hasOwnProperty(address);
+};
+
+const decimalsToUnit = (decimals) => {
+  const units = {
+    1: "wei",
+    3: "kwei",
+    6: "mwei",
+    9: "gwei",
+    12: "szabo",
+    15: "finney",
+    18: "ether",
+    21: "kether",
+    24: "mether",
+    27: "gether",
+    30: "tether",
+  };
+  return units[decimals];
+};
+
+module.exports = {
+  ...appConfig,
+  selectTrackedToken,
+  getToken0,
+  getToken1,
+  hasActiveSubscription,
+  addressInSubscription,
+  swapHanlder,
+  UniswapV2Pair,
+  wss,
+  fromWei: Web3.utils.fromWei,
+  decimalsToUnit,
+};
