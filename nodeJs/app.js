7ac9f03e35fe640c65ceaedad39cb668af9a24e0
sven3270350 <sven3270350@gmail.com>
2022-08-16 01:30:23 +0000

    use countdown listener

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -1,9 +1,10 @@
 require("dotenv").config({ path: "../.env" });
 
 const allBuysListener = require("./listeners/allBuys");
+const countdownListener = require("./listeners/countdown");
 
 // main function, iterates through all contracts in pairs
 (async function () {
   await allBuysListener.main();
+  await countdownListener.main();
 })();
-
