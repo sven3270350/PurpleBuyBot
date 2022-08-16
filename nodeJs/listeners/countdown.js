bf017fe7d0be62822184e790a612083a72a6c8ae
sven3270350 <sven3270350@gmail.com>
2022-08-16 01:30:14 +0000

    add countdown listener

---
+++ nodeJs/listeners/countdown.js
@@ -0,0 +1,73 @@
+const queries = require("../db/queries");
+const utils = require("../utils");
+const { countdowToStartTemplate } = require("../utils/templates");
+
+const countdowns = {};
+
+const countdownHandler = async (activeCampaign) => {
+  const startDate = new Date(activeCampaign.start_time);
+  const endDate = new Date(activeCampaign.end_time);
+
+  const startCountdownString = utils.getCountdownString(startDate);
+  const endCountDownString = utils.getCountdownString(endDate);
+
+  const ad = await utils.getAd(activeCampaign.group_id);
+
+  const startCountdownToUse =
+    new Date() > startDate ? "Started" : startCountdownString;
+
+  const templates = countdowToStartTemplate(
+    startCountdownToUse,
+    endCountDownString,
+    ad
+  );
+
+  // send message to group
+  utils.sendHTMLMessage(activeCampaign.group_id, templates);
+};
+
+const main = async (interval = 1000 * 30) => {
+  // get all tracked tokens
+
+  setInterval(async () => {
+    const activeCampaigns = await queries.getAllUpcomingCampaigns();
+
+    // if no active campaigns, and countdowns, then stop all countdowns
+    if (activeCampaigns.length === 0 && Object.keys(countdowns).length > 0) {
+      Object.keys(countdowns).forEach((id) => {
+        clearInterval(countdowns[id]);
+        delete countdowns[id];
+      });
+    }
+
+    // stop countdowns that are no longer active
+    Object.keys(countdowns).forEach((id) => {
+      const campaign = activeCampaigns.find((c) => Number(c.id) === Number(id));
+      if (!campaign) {
+        clearInterval(countdowns[id]);
+        delete countdowns[id];
+      }
+    });
+
+    // for each active campaign, create a countdown
+    activeCampaigns.forEach(async (activeCampaign) => {
+      const countdownInterval = 1000 * 60 * (activeCampaign.count_down ?? 10);
+
+      // check if it's is not in countdowns
+      if (!utils.keyInObject(activeCampaign.id, countdowns)) {
+        // create a countdown
+        const countdown = setInterval(async () => {
+          // send reminder message to group
+          await countdownHandler(activeCampaign);
+        }, countdownInterval);
+
+        // add to countdowns
+        countdowns[activeCampaign.id] = countdown;
+      }
+    });
+  }, interval);
+};
+
+module.exports = {
+  main,
+};
