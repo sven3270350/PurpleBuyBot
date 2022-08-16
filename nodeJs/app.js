84996bf074e5eed2b8d9ba6bc0f543b62a021e83
sven3270350 <sven3270350@gmail.com>
2022-08-16 15:46:07 +0000

    run campaign listener

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -2,9 +2,11 @@ require("dotenv").config({ path: "../.env" });
 
 const allBuysListener = require("./listeners/allBuys");
 const countdownListener = require("./listeners/countdown");
+const campaignBuyListener = require("./listeners/campaingBuys");
 
 // main function, iterates through all contracts in pairs
 (async function () {
   await allBuysListener.main();
   await countdownListener.main();
+  await campaignBuyListener.main();
 })();
