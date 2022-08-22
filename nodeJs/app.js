bed9de6db97523db212a9c70d4103320586006f9
sven3270350 <sven3270350@gmail.com>
2022-08-22 18:36:24 +0000

    include winner announcer

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -3,10 +3,12 @@ require("dotenv").config({ path: "../.env" });
 const allBuysListener = require("./listeners/allBuys");
 const countdownListener = require("./listeners/countdown");
 const campaignBuyListener = require("./listeners/campaingBuys");
+const winnerAnnouncer = require("./listeners/winnerAnnouncer");
 
 // main function, iterates through all contracts in pairs
 (async function () {
   await allBuysListener.main();
   await countdownListener.main();
   await campaignBuyListener.main();
+  await winnerAnnouncer.main();
 })();
