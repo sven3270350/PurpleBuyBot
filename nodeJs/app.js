f4163ded8c6914b045bea34837cf418d6d427e52
sven3270350 <sven3270350@gmail.com>
2022-08-26 20:29:28 +0000

    init price fetch

--- nodeJs/app.js
+++ nodeJs/app.js
@@ -4,9 +4,10 @@ const allBuysListener = require("./listeners/allBuys");
 const countdownListener = require("./listeners/countdown");
 const campaignBuyListener = require("./listeners/campaingBuys");
 const winnerAnnouncer = require("./listeners/winnerAnnouncer");
-
+const utils = require("./utils");
 // main function, iterates through all contracts in pairs
 (async function () {
+  await utils.cachePrices();
   await allBuysListener.main();
   await countdownListener.main();
   await campaignBuyListener.main();
