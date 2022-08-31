78570a36a44347aac983fd411694be7a94c1fc60
sven3270350 <sven3270350@gmail.com>
2022-08-31 15:56:57 +0000

    reset app

--- bot/app.py
+++ bot/app.py
@@ -24,6 +24,25 @@ migrate = Migrate(app, db)
 app.secret_key = AppConfigs().get_secret_key()
 
 
+@app.route('/')
+def get_info(word):
+
+    url = 'https://api.dictionaryapi.dev/api/v2/entries/en/{}'.format(word)
+
+    response = requests.get(url)
+
+# return a custom response if an invalid word is provided
+    if response.status_code == 404:
+        error_response = 'We are not able to provide any information about your word. Please confirm that the word is ' \
+                         'spelled correctly or try the search again at a later time.'
+        return error_response
+
+    data = response.json()[0]
+
+    print(data)
+    return data
+
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
