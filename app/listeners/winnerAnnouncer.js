25d54de166d12f8515b1b8a39cc75c76bcd02e50
sven3270350 <sven3270350@gmail.com>
2022-09-17 17:22:41 +0000

    last buy

--- app/listeners/winnerAnnouncer.js
+++ app/listeners/winnerAnnouncer.js
@@ -13,29 +13,50 @@ const announcerHandler = async (activeCampaign) => {
 
     let templates;
 
-    if (activeCampaign.campaing_type === "Biggest Buy") {
-      // anounce biggest buy campaign winner
-      const winner = await queries.getTop5Buys(activeCampaign.id);
-      const winnerAddress = winner[0].buyer_address;
-      templates = winnerBiggestBuysTemplate(winner, activeCampaign, ad);
-      await queries.deleteNonTop5Buys(activeCampaign.id);
+    if (activeCampaign.campaing_type) {
+      let winner, winnerAddress;
 
-      // write winner to campaign
-      await queries.writeWinnerToCampaign(winnerAddress, activeCampaign.id);
-    } else {
-      // anounce raffle campaign winner
-      const winner = await queries.getRandomWinner(activeCampaign.id);
-      const winnerAddress = winner.buyer_address;
-      templates = winnerRaffleBuysTemplate(winner, activeCampaign, ad);
+      switch (activeCampaign.campaing_type) {
+        case "Biggest Buy":
+          // anounce biggest buy campaign winner
+          winner = await queries.getTop5Buys(activeCampaign.id);
+          winnerAddress = winner[0].buyer_address;
+          templates = winnerBiggestBuysTemplate(winner, activeCampaign, ad);
+          await queries.deleteNonTop5Buys(activeCampaign.id);
 
-      // write winner to campaign
-      await queries.writeWinnerToCampaign(winnerAddress, activeCampaign.id);
+          // write winner to campaign
+          await queries.writeWinnerToCampaign(winnerAddress, activeCampaign.id);
+          break;
+        case "Raffle":
+          // anounce raffle campaign winner
+          winner = await queries.getRandomWinner(activeCampaign.id);
+          winnerAddress = winner.buyer_address;
+          templates = winnerRaffleBuysTemplate(winner, activeCampaign, ad);
 
-      await queries.deleteNonRandomWinner(activeCampaign.id);
+          // write winner to campaign
+          await queries.writeWinnerToCampaign(winnerAddress, activeCampaign.id);
+
+          await queries.deleteNonRandomWinner(activeCampaign.id);
+          break;
+        case "Last Buy":
+          // anounce last buy campaign winner
+          winner = await queries.getLastBuy(activeCampaign.id);
+          winnerAddress = winner.buyer_address;
+
+          templates = winnerRaffleBuysTemplate(winner, activeCampaign, ad);
+
+          // write winner to campaign
+          await queries.writeWinnerToCampaign(winnerAddress, activeCampaign.id);
+
+          await queries.deleteNonLastBuy(activeCampaign.id);
+          break;
+        default:
+          break;
+      }
+
+      // send message to group
+      utils.sendHTMLMessage(activeCampaign.group_id, templates);
     }
-
-    // send message to group
-    utils.sendHTMLMessage(activeCampaign.group_id, templates);
   } catch (error) {
     console.log("[announcer::announcerHandler]", error);
   }
@@ -60,9 +81,10 @@ const main = async (interval = 1000 * 30) => {
                 // send reminder message to group
                 await announcerHandler(activeCampaign);
                 // delete announcer
+                clearTimeout(announcers[activeCampaign.id]);
                 delete announcers[activeCampaign.id];
                 if (timeDiff > maxDelayValue) {
-                 await queries.stopCampaingByGroup(activeCampaign.id);
+                  await queries.stopCampaingByGroup(activeCampaign.id);
                 }
               },
               timeDiff > maxDelayValue ? maxDelayValue : timeDiff
