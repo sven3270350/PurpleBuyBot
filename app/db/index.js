a8f3c815d6b012815f019e93a1c6879bf48167f2
sven3270350 <sven3270350@gmail.com>
2023-02-28 10:40:09 +0000

    limit db connection to 3 and use transactions

--- app/db/index.js
+++ app/db/index.js
@@ -1,10 +1,12 @@
 const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  max: 3,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
