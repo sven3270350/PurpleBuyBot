de8ebd423f432b85bdb9038fc50f0f4033089bc5
Sauli <sauli.savinainen@gmail.com>
2023-05-05 00:19:58 +0300

    pool uses a singleton pattern instead of creating a new pool connection instead of creating new pools

--- app/db/index.js
+++ app/db/index.js
@@ -1,29 +1,57 @@
 const { Pool } = require("pg");
 console.log("Database env var test:", process.env.DATABASE_URL);
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  max: 3,
-  ssl: {
-    rejectUnauthorized: false, //this has to be set true in production -s
-  },
-});
 
-pool.on('connect', (client) => {
-  console.log('!!Database connection successful!!');
-});
+let pool;
 
-pool.on('error', (err) => {
-  console.error('!!Error establishing the DB connection!!', err)
-  process.exit(-1)
-})
+function createPool() {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    max: 3,
+    ssl: {
+      rejectUnauthorized: false, //this has to be set true in production -s
+    },
+  });
 
+  pool.on('connect', (client) => {
+    console.log('!!Database connection successful!!');
+  });
+
+  pool.on('error', (err) => {
+    console.error('!!Error establishing the DB connection!!', err)
+    process.exit(-1)
+  });
+
+  return pool;
+}
+
+// Export a singleton instance of the connection pool
 module.exports = {
   query: (text, params) => {
-    return pool.query(text, params)
+    if (!pool) {
+      createPool();
+    }
+    return pool.connect()
+      .then(client => {
+        return client.query(text, params)
+          .then(result => {
+            client.release()
+            return result
+          })
+          .catch(err => {
+            client.release()
+            console.error('Error executing query', err)
+            throw err
+          })
+      })
       .catch(err => {
-        console.error('Error executing query', err);
-        throw err;
-      });
+        console.error('Error acquiring client from pool', err)
+        throw err
+      })
   },
-  pool: pool,
+  pool: () => {
+    if (!pool) {
+      createPool();
+    }
+    return pool;
+  },
 };
