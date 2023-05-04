54f85d8ce7bb8a0e75e73cbfb1e1fb7e0c56e610
Sauli <sauli.savinainen@gmail.com>
2023-05-04 19:54:33 +0300

    removed clustering

--- app/app.js
+++ app/app.js
@@ -1,5 +1,3 @@
-const cluster = require("cluster");
-const semver = require("semver");
 const { startMainJobs, startChildJobs } = require("./schedule");
 
 const allBuysListener = require("./listeners/allBuys");
@@ -15,48 +13,18 @@ http.globalAgent.maxSockets = 100;
 
 const main = async () => {
   try {
-    const isPrimaryCluster = () => {
-      if (semver.gte(process.version, "v16.0.0")) {
-        return cluster.isPrimary;
-      }
-      return cluster.isMaster;
-    };
+    // start main jobs
+    startMainJobs();
 
-    if (isPrimaryCluster()) {
-      // start main jobs
-      startMainJobs();
+    await Promise.all([
+      allBuysListener.main(),
+      campaignBuyListener.main(),
+      countdownListener.main(),
+      winnerAnnouncer.main(),
+    ]).catch((err) => console.log("[All::PromiseError] : ", err));
 
-      // await allBuysListener.main();
-      // await campaignBuyListener.main();
-      // await countdownListener.main();
-      // await winnerAnnouncer.main();
-
-      await Promise.all([
-        allBuysListener.main(),
-        campaignBuyListener.main(),
-        countdownListener.main(),
-        winnerAnnouncer.main(),
-      ]).catch((err) => console.log("[All::PromiseError] : ", err));
-
-      // manage child processes
-      let childProcessId;
-      cluster.on("exit", (worker, code, signal) => {
-        // refork
-        if (worker.process.id == childProcessId) {
-          console.log(
-            `[app::main] child process ${childProcessId} died, reforking...`
-          );
-          cluster.fork();
-        }
-      });
-      cluster.on("fork", (worker) => {
-        childProcessId = worker.process.pid;
-      });
-      cluster.fork();
-    } else {
-      // start child jobs in child process
-      startChildJobs();
-    }
+    // start child jobs
+    startChildJobs();
   } catch (error) {
     console.log("[_app::main]", error);
   }
