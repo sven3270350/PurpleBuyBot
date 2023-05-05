26747a8ca262780070d41e9608b693814b7bc33f
Sauli <sauli.savinainen@gmail.com>
2023-05-05 18:51:47 +0300

    rollback as commit, cannot connect to the websocket

--- app/app.js
+++ app/app.js
@@ -1,6 +1,4 @@
 const { startMainJobs, startChildJobs } = require("./schedule");
-const TelegramBot = require('node-telegram-bot-api');
-const WebSocket = require('ws');
 
 const allBuysListener = require("./listeners/allBuys");
 const countdownListener = require("./listeners/countdown");
@@ -13,32 +11,16 @@ const http = require("http");
 https.globalAgent.maxSockets = 100;
 http.globalAgent.maxSockets = 100;
 
-const botToken = '5749218942:AAHScJvWi3GuLfGv-UvKumedwz-9wRcgJgM';
-const bot = new TelegramBot(botToken, { polling: false });
-
-const wsEndpoint = 'wss://api.telegram.org/bot' + botToken;
-const ws = new WebSocket(wsEndpoint);
-
-ws.on('open', () => {
-  console.log('WebSocket connection established');
-});
-
-ws.on('message', (data) => {
-  const update = JSON.parse(data);
-  console.log('Received update:', update);
-  // Process the update as needed
-});
-
 const main = async () => {
   try {
     // start main jobs
     startMainJobs();
 
     await Promise.all([
-      allBuysListener.main(bot),
-      campaignBuyListener.main(bot),
-      countdownListener.main(bot),
-      winnerAnnouncer.main(bot),
+      allBuysListener.main(),
+      campaignBuyListener.main(),
+      countdownListener.main(),
+      winnerAnnouncer.main(),
     ]).catch((err) => console.log("[All::PromiseError] : ", err));
 
     // start child jobs
